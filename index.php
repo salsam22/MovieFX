41d8fe00e4a076a930daee92f1247b4401ad9f3d
salsam22 <76152023+salsam22@users.noreply.github.com>
2021-11-08 12:16:32 +0100

    MovieFX - Cookie de last_insert

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
 declare(strict_types=1);
-
+setcookie ("last_visit_date", (string) time(), time() + 604800);
 require "src/Movie.php";
+if(isset($_COOKIE['last_visit_date'])) {
+    $seconds = $_COOKIE["last_visit_date"];
+    if (filter_var($seconds,FILTER_VALIDATE_INT)) {
+        echo "<h1>Benvingut,  la seua ultima visita ha sigut: ".date("d/m/Y H:i:s", (int)$seconds)."</h1>";
+    } else {
+        echo "<h1>Has modificat manualment la cookie de last_visit_date.</h1>";
+    }
 
-$pdo = new PDO("mysql:host=localhost;dbname=moviefx;charset=utf8", "dbuser", "1234");
+} else {
+    echo "<h1>Benvingut!!</h1>";
+}
+
+
+    $pdo = new PDO("mysql:host=localhost;dbname=moviefx;charset=utf8", "dbuser", "1234");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $moviesStmt = $pdo->prepare("SELECT * FROM movie");
