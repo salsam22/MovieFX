29863b1e250beabff2584796c71109243f26148d
salsam22 <76152023+salsam22@users.noreply.github.com>
2021-11-24 20:21:37 +0100

    601 i 603

---
+++ FlashMessage.php
@@ -0,0 +1,16 @@
+<?php
+class FlashMessage {
+    const SESSION_KEY = "flash-message";
+
+    public static function get(string $key, $defaultValue = "") {
+
+    }
+
+    public static function set(string $key, $value) {
+
+    }
+
+    private static function unset(string $key) {
+
+    }
+}
